5d524959283ba957aced558df2cf5f1f1db338b0
zzmp <zmp@umich.edu>
2014-09-16 02:00:55 -0700

    (refactor) Elucidate next steps

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ var open = require('open');
 prompt({
   type: 'list',
   name: 'step',
-  message: 'Which step are you on (start with 1)?',
-  choices: ['1', '2', '3', '4'],
-  default: '1'
+  message: 'Which step are you on?',
+  choices: [
+    { name: 'Introduction: MVC', value: 0 },
+    { name: 'Create a MODEL', value: 1 },
+    { name: 'Render a Model: the VIEW', value: 2 },
+    { name: 'Interactions: the CONTROLLER', value: 3 },
+    { name: 'Building an entire application', value: 4 },
+    { name: 'Going fullstack: interacting with your server', value: 5 }
+  ]
 }, function(answers) {
   // Serve specified step
   app.set('view engine', 'html');
